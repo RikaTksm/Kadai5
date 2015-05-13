d0e04f4d6e168e4598b78e8e7c4a69f3019d0413
g13935nm <g13935nm@m20209.tsuda.ac.jp>
2015-05-13 16:01:39 +0900

    ドライバー　ryuy(3回目) ナビゲーター　RikaTksm 100から999まで完了

--- src/inttoeng/IntToEng.java
+++ src/inttoeng/IntToEng.java
@@ -23,12 +23,31 @@ public class IntToEng {
     			"ten", "eleven", "twelve", "thirteen", "fourteen",
     			"fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
     	//20-90‚Ì10‚ÌˆÊ
-    	String[] eNum2={"twenty", "thirty", "forty", "fifty", "sixty",
+    	String[] eNum2={"","twenty", "thirty", "forty", "fifty", "sixty",
     			"seventy", "eighty", "ninety"};
+    	
     	//20^99‚Ü‚Å‚Ì1‚ÌˆÊ
     	String[] eNum3={"","one", "two", "three", "four",
     			"five", "six", "seven", "eight", "nine"};
     	
+    	//100^999‚Ü‚Å‚Ì100‚ÌˆÊ
+    	String[] eNum4={"onehundred", "twohundred", "threehundred", "fourhundred",
+    			"fivehundred", "sixhundred", "sevenhundred", "eighthundred", "ninehundred"};
+    	
+    	//100^999‚Ü‚Å‚Ì10‚ÌˆÊ
+    	String[] eNum5={"","twenty", "thirty", "forty", "fifty", "sixty",
+    			"seventy", "eighty", "ninety"};
+    	
+    	//100^999‚Ü‚Å‚Ì1‚ÌˆÊ
+    	String[] eNum6={"", "one", "two", "three", "four",
+    			"five", "six", "seven", "eight", "nine"};
+    	
+    	//100^999‚Ü‚Å‚Ì1‚ÌˆÊ
+    	String[] eNum7={"ten", "eleven", "twelve", "thirteen", "fourteen",
+		"fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+    	
+    	
+    			
     	String num = String.valueOf(n);
     	if(n < 20){ 
     		num=eNum1[n];
@@ -41,10 +60,48 @@ public class IntToEng {
     		int num10a = Integer.parseInt(num10);
     		int num1a = Integer.parseInt(num1);
     		
-    		num=eNum2[num10a-2]+eNum3[num1a];
+    		num=eNum2[num10a-1]+eNum3[num1a];
     			return num;
     		}
-    	
+    	if(n>=100 && n<=109){
+    		String num100 = num.substring(0,1);//100‚ÌˆÊ
+    		String num10b = num.substring(1,2);//10‚ÌˆÊ
+    		String num1b = num.substring(2,3);//1‚ÌˆÊ
+    		
+    		
+    		int num100b = Integer.parseInt(num100);
+    		int num10c = Integer.parseInt(num10b);
+    		int num1c = Integer.parseInt(num1b);
+    		
+    		num=eNum4[num100b-1]+eNum5[num10c]+eNum6[num1c];
+    			return num;
+    	}
+    	if(n>=110 && n<=119){
+    		String num100 = num.substring(0,1);
+    		String num10b = num.substring(1,2);
+    		String num1b = num.substring(2,3);
+    		
+    		
+    		int num100b = Integer.parseInt(num100);
+    		int num10c = Integer.parseInt(num10b);
+    		int num1c = Integer.parseInt(num1b);
+    		
+    		num=eNum4[num100b-1]+eNum5[num10c-1]+eNum7[num1c];
+    			return num;
+    	}
+    	if(n>=120 && n<=999){
+    		String num100 = num.substring(0,1);
+    		String num10b = num.substring(1,2);
+    		String num1b = num.substring(2,3);
+    		
+    		
+    		int num100b = Integer.parseInt(num100);
+    		int num10c = Integer.parseInt(num10b);
+    		int num1c = Integer.parseInt(num1b);
+    		
+    		num=eNum4[num100b-1]+eNum5[num10c-1]+eNum6[num1c];
+    			return num;
+    	}
         return "";
     }
 }
